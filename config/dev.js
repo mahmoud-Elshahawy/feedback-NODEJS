7554251fb979b12abf4f10b51af77cc5c396085f
Mahmoud Hamdy <engmahmoudhamdy63277@gmail.com>
2022-02-21 11:36:58 +0200

    Create dev.js
    Changes

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID : '293499341419-5fclecmgp7nb1ic2u5vv91d69p2mcn8u.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://user:finicom4ever@cluster0.aahwc.mongodb.net/nodejs-dev?retryWrites=true&w=majority',
+    cookieKey: 'ashjkgksdfhkahsjkdhsafdasd'
+};
